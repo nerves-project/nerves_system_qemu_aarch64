2ea535c4f05f3b899778c97ba6dcc5a998d846e2
Frank Hunleth <fhunleth@troodon-software.com>
2025-11-01 14:05:15 -0400

    Fix automatic firmware path detection

--- lib/mix/tasks/nerves.gen.qemu.ex
+++ lib/mix/tasks/nerves.gen.qemu.ex
@@ -16,8 +16,8 @@ defmodule Mix.Tasks.Nerves.Gen.Qemu do
           fw_path
 
         [] ->
-          [fw_path] = File.ls!("#{Mix.Project.build_path()}/nerves/images")
-          fw_path
+          config = Mix.Project.config()
+          Path.expand("#{Mix.Project.build_path()}/nerves/images/#{config[:app]}.fw")
 
         _ ->
           Mix.shell().error(
